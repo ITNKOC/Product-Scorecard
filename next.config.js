a29d634e8691cba8e13d759c766eced56461d248
ITNKOC <koceila.djaballah@gmail.com>
2025-07-13 01:07:07 -0400

    new update

--- next.config.js
+++ next.config.js
@@ -9,29 +9,14 @@ const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ['@prisma/client', 'prisma']
   },
-  webpack: (config, { isServer, buildId }) => {
+  webpack: (config, { isServer }) => {
     if (isServer) {
       config.externals.push('@prisma/client')
       config.externals.push('prisma')
     }
     
     return config
-  },
-  
-  // Disable build tracing for API routes during build
-  outputFileTracing: false,
-  
-  // Disable static optimization for API routes
-  async rewrites() {
-    return []
-  },
-  
-  // Exclude API routes from static generation
-  generateStaticParams: false,
-  
-  // Skip build-time page data collection for dynamic routes
-  skipMiddlewareUrlNormalize: true,
-  skipTrailingSlashRedirect: true
+  }
 }
 
 module.exports = nextConfig
